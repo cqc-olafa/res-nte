77bb01b7d724533bf11f5d88f8c7928eddc944f2
cqc-olafa <qiuchiao0920@gmail.com>
2025-07-18 03:48:26 +0800

    modified:   download.py 	modified:   remote/fabfile.py

--- download.py
+++ download.py
@@ -1,5 +1,12 @@
 import kagglehub
+import argparse
+def download_wich(setname):
+    path = kagglehub.dataset_download(setname ="deadskull7/fer2013", path="~/.data")
 
-path = kagglehub.dataset_download("deadskull7/fer2013", path="/.data")
-
-print("Path to dataset files:", path)
+    print("Path to dataset files:", path)
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--setname",  required=True,
+                        help="e.g. deadskull7/fer2013")
+    args = parser.parse_args()
+    download_wich(args.setname)
--- remote/fabfile.py
+++ remote/fabfile.py
@@ -154,6 +154,16 @@ def run_train(c, user, epochs=10, batch_size=64, lr=0.05):
         )
     conn.close()
 @task
+def load_data(c, user, setname):
+    conn = establish_base_connection(user)
+    with conn.cd(remote_dir):
+        conn.run('pip install -r requirements.txt', warn=True)
+        conn.run(
+            f'python download.py'
+            f'--setname {setname}'
+            pty = True
+            )    
+@task
 def deploy(c, user):
 
     """
